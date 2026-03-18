e2ea83128b7d755da73d9f29ef33eea19fa70bc2
Draco <62447702+Dracorex2@users.noreply.github.com>
2026-03-18 15:12:35 +0100

    fini?

--- src/client/client_read.c
+++ src/client/client_read.c
@@ -1,23 +1,26 @@
 #include "client.h"
 #include "common.h"
 #include <stdio.h>
+#define TRY 5
 
 void client_read(int fd, packet_t packet) {
 	char buf[PACKET_LEN] = {0};
 	pack_packet(buf, packet);
-	if (sendto(fd, &buf, PACKET_LEN, 0, (void *)&packet.addr, sizeof(packet.addr)) < 0) {
-		perror("send error");
-		return;
-	}
-
-	struct pollfd pfd;
-	pfd.fd = fd;
-	pfd.revents = 0;
-	pfd.events = POLLIN;
-	poll(&pfd, 1, -1);
-	if (pfd.revents & POLLIN) {
-		int n = recv(fd, buf, PACKET_LEN, 0);
-		unpack_packet(&packet, buf, n);
-		printf("%s\n", packet.data);
+	for (int i = 0; i < TRY; i++) {
+		if (sendto(fd, &buf, PACKET_LEN, 0, (void *)&packet.addr, sizeof(packet.addr)) < 0) {
+			perror("send error");
+			return;
+		}
+		struct pollfd pfd;
+		pfd.fd = fd;
+		pfd.revents = 0;
+		pfd.events = POLLIN;
+		poll(&pfd, 1, 500);
+		if (pfd.revents & POLLIN) {
+			int n = recv(fd, buf, PACKET_LEN, 0);
+			unpack_packet(&packet, buf, n);
+			printf("%s\n", packet.data);
+			break;
+		}
 	}
 }
